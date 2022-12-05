2b2aa95a5063937479aaf93fd05bcaca389387f1
Andrew <andrewmgunn26@gmail.com>
2022-12-05 14:33:28 -0600

    finished with swipe

--- TrueFalseQuiz/TrueFalseQuiz/MainPage.xaml.cs
+++ TrueFalseQuiz/TrueFalseQuiz/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -11,6 +12,9 @@ namespace TrueFalseQuiz
     public partial class MainPage : ContentPage
     {
         public int i;
+        public int j;
+        IList<string> strList = new List<string>();
+        IList<string> imageList = new List<string>();
         public MainPage()
         {
             InitializeComponent();
@@ -18,16 +22,27 @@ namespace TrueFalseQuiz
             falseButton.IsVisible = true;
             questionArea.IsVisible = true;
             i = 0;
+            j = 0;
+            strList.Add("A");
+            strList.Add("B");
+            strList.Add("C");
+            strList.Add("D");
+            strList.Add("E");
+            imageList.Add("a.jpg");
+            imageList.Add("b.png");
+            imageList.Add("c.jpg");
+            imageList.Add("d.jpg");
+            imageList.Add("e.jpg");
         }
 
         List<Question> questions = new List<Question>()
         {
-            new Question("The glass is always half full."),
-            new Question("There is always a good side to any bad outcome."),
-            new Question("People are generally good in nature."),
-            new Question("You always trust things will end up okay."),
-            new Question("Sometimes people can't change."),
-            new Question("Some people are a lost cause.")
+            new Question("The glass is always half full.", "a.jpg"),
+            new Question("There is always a good side to any bad outcome.", "b.png"),
+            new Question("People are generally good in nature.", "c.jpg"),
+            new Question("You always trust things will end up okay.", "d.jpg"),
+            new Question("Sometimes people can't change.", "e.jpg"),
+            new Question("Some people are a lost cause.", "b.png"),
         };
 
 
@@ -58,7 +73,7 @@ namespace TrueFalseQuiz
         void ValidateNum()
         {
  
-            if (i == 5)
+            if (i >= 5)
             {
                 trueButton.IsVisible = false;
                 falseButton.IsVisible = false;
@@ -82,7 +97,48 @@ namespace TrueFalseQuiz
 
             }
         }
-  
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+
+            i++;
+
+            theLabel2.Text = e.Direction.ToString() + " " + j;
+            if (e.Direction == SwipeDirection.Right)
+            {
+                if (j >= strList.Count - 1)
+                {
+                   j = -1;
+                }
+                theLabel2.Text = strList[++j];
+                theImage.Source = imageList[j];
+                ValidateNum();
+            }
+            else if (e.Direction == SwipeDirection.Left)
+            {
+                if (j <= 0)
+                {
+                    j = strList.Count;
+                }
+            
+                theLabel2.Text = strList[--j];
+                theImage.Source = imageList[j];
+                ValidateNum();
+            }
+            else if (e.Direction == SwipeDirection.Up)
+            {
+
+                theLabel2.Text = "Oh Snap";
+
+            }
+            else if (e.Direction == SwipeDirection.Down)
+            {
+
+                theLabel2.Text = "Wake up!";
+
+            }
+        }
 
     }
+
 }
+
--- TrueFalseQuiz/TrueFalseQuiz/Question.cs
+++ TrueFalseQuiz/TrueFalseQuiz/Question.cs
@@ -9,11 +9,12 @@ namespace TrueFalseQuiz
      class Question
     {
         public string Text { get; set; }
+        public string Image { get; set; }
 
-
-        public Question(string text)
+        public Question(string text, string image)
         {
             this.Text = text;
-        } 
+            this.Image = image;
+        }
     }
 }
